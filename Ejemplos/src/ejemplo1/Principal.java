2bf7cd400548d2fbcad57dfaf5d2dff0cbd5907d
Roberto <robertorebolledo151@gmail.com>
2023-03-13 10:59:06 +0100

    pro

--- Ejemplos/src/ejemplo1/Principal.java
+++ Ejemplos/src/ejemplo1/Principal.java
@@ -5,15 +5,31 @@ import utilidades.Leer;
 public class Principal {
 	
 	public static void main(String[] args) {
-		int num=10,de;
+		int num=10,de=0;
 		double res;
-		
+
 		do {
-			de=Leer.datoInt();
-		}while(de==0);
-		res=num/de;
-		System.out.println(res);		
-		
+			try {
+				System.out.println("Numerador");
+				num=Leer.datoInt();
+				System.out.println("denominador");
+				de=Leer.datoInt();
+				res=num/de;
+				System.out.println("Resultado:"+res);
+				
+			}catch(ArithmeticException e){
+				
+				System.out.println("Esta dividiendo por 0");
+				
+			}catch(NumberFormatException  e) {
+				
+				System.out.println("Te he pedido un numero");
+				
+			}catch(Exception e) {
+				
+				System.out.println("Error inesperado");
+			}
+		}while(num<0||de<=0);
 	}
 	
 }	
